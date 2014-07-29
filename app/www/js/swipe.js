e162d447a3f063f3aaf1a6f60843c2e39c7e593d
Zak Cutner <zcutner@me.com>
2014-07-29 23:13:11 +0100

    Fixed some bugs

--- app/www/js/swipe.js
+++ app/www/js/swipe.js
@@ -1,18 +1,12 @@
 var swipe = false;
 var colours = ['A87EDF', '89E18A', 'D26B4A', '7BD0C6'];
 
-for(var i = 0; i < 40; i++) {
-	$('li:nth-child(2)').after('<li><div class="tweet"><div class="icon-outer"><div class="icon-middle"><img class="icon"/></div></div><div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu felis eget enim iaculis bibendum eget id felis. Integer risus cras amet.</div></div></li>');
-}
-
-$('img.icon').each(function(index) {
-	var url = 'http://identicon.org/?t=' + Math.random().toString(36).substr(2, 5) + '&s=50&c=' + colours[index % colours.length];
-	$(this).attr('src', url);
-	console.log();
-});
-
 $(function() {
-	$('body').swipe({
+	for(var i = 0; i < 40; i++) {
+		$('li:nth-child(2)').after('<li><div class="tweet"><div class="icon-outer"><div class="icon-middle"><img class="icon"/></div></div><div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu felis eget enim iaculis bibendum eget id felis. Integer risus cras amet.</div></div></li>');
+	}
+	
+	$('html').swipe({
 		swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
 			if((direction == 'left' || direction == 'right') && direction != swipe) {
 				if(swipe) {
@@ -33,10 +27,17 @@ $(function() {
 			else if(direction == 'up') {
 				$('.tweet.active').parent().animate({marginTop: '-131px'}, 200, function() {
 					$(this).remove();
+					swipe = false;
 					$('.tweet').first().addClass('active').after('<div class="tick"></div><div class="cross"></div>');
 				});
 			}
 		},
 		threshhold: 0
+	});
+
+	$('img.icon').each(function(index) {
+		var url = 'http://identicon.org/?t=' + Math.random().toString(36).substr(2, 5) + '&s=30&c=' + colours[index % colours.length];
+		$(this).attr('src', url);
+		console.log();
 	});
 });
