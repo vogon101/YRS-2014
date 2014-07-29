5a287444dffb3b6817e7e708c98203091de6c5cc
Zak Cutner <zcutner@me.com>
2014-07-29 23:21:26 +0100

    Added view for mobile testing

--- app/www/js/swipe.js
+++ app/www/js/swipe.js
@@ -1,11 +1,11 @@
 var swipe = false;
-var colours = ['A87EDF', '89E18A', 'D26B4A', '7BD0C6'];
+var colours = ['a87edf', '89e18a', 'd26b4a', '7bd0c6'];
 
 $(function() {
 	for(var i = 0; i < 40; i++) {
 		$('li:nth-child(2)').after('<li><div class="tweet"><div class="icon-outer"><div class="icon-middle"><img class="icon"/></div></div><div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu felis eget enim iaculis bibendum eget id felis. Integer risus cras amet.</div></div></li>');
 	}
-	
+
 	$('html').swipe({
 		swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
 			if((direction == 'left' || direction == 'right') && direction != swipe) {
