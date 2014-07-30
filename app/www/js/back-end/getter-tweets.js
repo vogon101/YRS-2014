bceea30b739e6464a4a7c772d349e1cb3327961e
vogon101 <vogonjeltz101@gmail.com>
2014-07-30 12:13:19 +0100

    Not Much

--- app/www/js/back-end/getter-tweets.js
+++ app/www/js/back-end/getter-tweets.js
@@ -79,6 +79,7 @@ function algo (data, callback) {
 	var done = [];
 	for (var i = 0; i < data.length; i++) {
 		var replace = "<span class='hide'>dshbfjhsdb</span>";
+
 		var search = new RegExp(data[i].user.name, "ig");
 		data[i].text = data[i].text.replace(search, replace);
 
