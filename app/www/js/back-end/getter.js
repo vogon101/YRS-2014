93569d4fee1d03af1dd4267e5b62a871a4a53f3e
vogon101 <vogonjeltz101@gmail.com>
2014-07-30 09:55:21 +0100

    Back-End Improvements

--- app/www/js/back-end/getter.js
+++ app/www/js/back-end/getter.js
@@ -1,7 +1,7 @@
 function start (users, callback) {
 	for (var i = 0; i < users.length; i++) {
-		get (users[i], function() {
-			callback();
+		get (users[i], function(error, done) {
+			callback(error, done);
 		});
 	}
 }
@@ -10,24 +10,26 @@ function get (user, callback) {
 	var url ="http://www.zakcutner.uk/whatcandidate/api.php?callback=?&name="+user;
 	console.log(url);
 
-	$.getJSON("http://www.zakcutner.uk/whatcandidate/api.php?callback=?&name="+user, function(data) {
-		trim (data, function() {
-			console.log(data);
-			callback();
+	$.getJSON(url, function(data) {
+		if (data.hasOwnProperty("errors")) {
+			callback (true);//error
+			return;
+		}
+		trim (data, function(error, done) {
+			callback(error, done);
 		});
 	});
 }
 
 function trim (data, callabck) {
-	console.log(data);
 
 	var done = [];
 	for (var i = 0; i < data.length; i++) {
 		done[i] = {"text":data[i].text, "id":data[i].id, "user":{"screen_name":data[i].user.screen_name, "name":data[i].user.name}};
 	}
 
-	algo (done, function () {
-		callback();
+	algo (done, function (error, done) {
+		callback(error, done);
 	});
 
 }
@@ -36,7 +38,6 @@ function algo (data, callback) {
 
 	var done = [];
 	for (var i = 0; i < data.length; i++) {
-		console.log (data[i].text);
 
 		var search = new RegExp(data[i].user.name, "ig");
 		data[i].text = data[i].text.replace(search, "----");
@@ -44,12 +45,11 @@ function algo (data, callback) {
 		var search = new RegExp(data[i].user.screen_name, "ig");
 		data[i].text = data[i].text.replace(search, "----");
 
-		console.log (data[i].text);
 		done.push(data[i]);
 	}
 
-	dbin(done, function () {
-		callback();
+	dbin(done, function (error, done) {
+		callback(error, done);
 	});
 
 }
@@ -58,8 +58,8 @@ function dbin (data, callback) {
 	
 	done = data;
 
-	console.log(data);
 
-	callback();
+
+	callback(false, done);//no errors
 
 }
